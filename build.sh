e30b7b8b00c16c0d60639c08e7728c57074df874
Nova Herawan <kadeksananda@gmail.com>
2025-06-04 12:08:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,7 @@
 set -e
 
 pip install -r requirements.txt
-python manage.py migrate --noinput
-
-# Create superuser if none exists
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.exists(): \
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
+python manage.py migrate
 
 python manage.py loaddata shoppit/fixtures/products.json
 python manage.py collectstatic --noinput
