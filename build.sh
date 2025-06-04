2b3cb059e2e63991e9013ac8078753f383b09dd6
Nova Herawan <kadeksananda@gmail.com>
2025-06-04 12:12:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 pip install -r requirements.txt
 python manage.py migrate
 
-python manage.py loaddata shoppit/fixtures/products.json
 python manage.py collectstatic --noinput
+python manage.py loaddata shoppit/fixtures/products.json
